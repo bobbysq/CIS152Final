4dfc379b3f82c5bc0bd494050bfea19c94f70a65
Bobby <bobbysq@gmail.com>
2020-11-07 23:30:39 -0600

    Initial commit, started laying out the player queue library

---
+++ README.md
@@ -0,0 +1,1 @@
+# CIS 152 Final Project
---
+++ player_queue.py
@@ -0,0 +1,12 @@
+from queue import PriorityQueue
+
+class PlayerQueue:
+    def __init__(self, player_dict = {}):
+        _q = PriorityQueue()
+        _player_dict = player_dict
+
+    def add_player(self, username):
+        pass
+
+    def get_next_player(self):
+        pass
