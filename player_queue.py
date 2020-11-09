5d2142855f50fe41cb73c28c3a5971e2de1485ea
Bobby <bobbysq@gmail.com>
2020-11-08 19:46:58 -0600

    Implemented add_player to PlayerQueue

--- player_queue.py
+++ player_queue.py
@@ -2,11 +2,16 @@ from queue import PriorityQueue
 
 class PlayerQueue:
     def __init__(self, player_dict = {}):
-        _q = PriorityQueue()
-        _player_dict = player_dict
+        self._q = PriorityQueue()
+        self._player_dict = player_dict
 
     def add_player(self, username):
-        pass
+        if username in self._player_dict:
+            times_played = self._player_dict[username]
+            self._q.put(username, times_played)
+        else:
+            self._q.put(username, 0)
+            self._player_dict[username] = 0
 
     def get_next_player(self):
         pass
