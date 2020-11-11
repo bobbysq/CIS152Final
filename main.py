e01bc0905009c3e9e52785623e063fd0e18cd506
Bobby <bobbysq@gmail.com>
2020-11-11 14:23:31 -0600

    Implemented get_next_player and made a basic driver

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-from player_queue import *
+from player_queue import PlayerQueue
+
+if __name__ == "__main__":
+    q = PlayerQueue()
+
+    q.add_player('test1')
+    q.add_player('test2')
+    q.add_player('test3')
+
+    print(q.get_next_player())
+    print(q.get_next_player())
+    print(q.get_next_player())
+
+    q.add_player('test4')
+    q.add_player('test2')
+    q.add_player('test1')
+
+    print(q.get_next_player())
+    print(q.get_next_player())
+    print(q.get_next_player())
--- player_queue.py
+++ player_queue.py
@@ -1,17 +1,23 @@
 from queue import PriorityQueue
 
+
 class PlayerQueue:
-    def __init__(self, player_dict = {}):
+    def __init__(self, player_dict={}):
         self._q = PriorityQueue()
         self._player_dict = player_dict
 
     def add_player(self, username):
         if username in self._player_dict:
             times_played = self._player_dict[username]
-            self._q.put(username, times_played)
+            self._q.put((times_played, username))
         else:
-            self._q.put(username, 0)
+            self._q.put((0, username))
             self._player_dict[username] = 0
 
     def get_next_player(self):
-        pass
+        if not self._q.empty():
+            next_player = self._q.get()[1]
+            self._player_dict[next_player] += 1
+            return next_player
+        else:
+            return None
