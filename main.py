2a020753d41189857ca7815964700ea4d1e55c68
Bobby <bobbysq@gmail.com>
2020-11-14 20:52:31 -0600

    Added JSON saving/loading to the program

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from player_queue import PlayerQueue
+import json
+from os import path
 
 if __name__ == "__main__":
-    q = PlayerQueue()
+    if path.exists('players.json'):
+        with open('players.json', 'r') as f:
+            player_data = json.load(f)
+    else:
+        f = open('players.json', 'w') #create file if it doesn't exist
+        f.close()
+        player_data = {}
+
+    q = PlayerQueue(player_data)
 
     q.add_player('test1')
     q.add_player('test1')
@@ -19,3 +29,10 @@ if __name__ == "__main__":
     print(q.get_next_player())
     print(q.get_next_player())
     print(q.get_next_player())
+
+    json_out = json.dumps(q.player_dict)
+
+    with open('players.json', 'w') as f:
+        json.dump(q.player_dict, f)
+
+    print(json_out)
