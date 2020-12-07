cc935f69ed5921682dbc10f3b0705f41411b55a9
Bobby <bobbysq@gmail.com>
2020-12-06 22:24:53 -0600

    Added menu to get next player

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 from os import path
 import configparser
 import twitch
+import sys
 
 config = configparser.ConfigParser()
 config.read('config_real.ini')
@@ -11,6 +12,7 @@ CLIENT_ID = config['DEFAULT']['ClientID']
 CLIENT_SECRET = config['DEFAULT']['ClientSecret']
 OAUTH = config['DEFAULT']['OAuth']
 CHANNEL = config['DEFAULT']['Channel']
+
 
 def handle_message(q, message: twitch.chat.Message) -> None:
     print(message.sender + ': ' + message.text)
@@ -22,17 +24,32 @@ def handle_message(q, message: twitch.chat.Message) -> None:
         q.add_player(message.sender)
         message.chat.send(message.sender + ', you are now in the queue')
 
-    if message.text.startswith('!next'):
-        #message.chat.send('/w ' + message.sender + ' the code is: [CODE HERE]')
-        if message.sender.lower() == CHANNEL.lower():
-            message.chat.send('Hello, ' + q.get_next_player() + '. The code is: [CODE HERE]')
+    # if message.text.startswith('!next'):
+    #     #message.chat.send('/w ' + message.sender + ' the code is: [CODE HERE]')
+    #     if message.sender.lower() == CHANNEL.lower():
+    #         message.chat.send('Hello, ' + q.get_next_player() + '. The code is: [CODE HERE]')
+
+
+def show_menu():
+    choice = 0
+    while choice < 1 or choice > 3:
+        print('PlayerQueueBot menu:\n\
+\n\
+1. Go to next player\n\
+2. Show top players\n\
+3. Quit')
+        choice = int(input('Please enter a selection: '))
+        if choice < 1 or choice > 3:
+            print('Invalid choice.')
+    return choice
+
 
 if __name__ == "__main__":
     if path.exists('players.json'):
         with open('players.json', 'r') as f:
             player_data = json.load(f)
     else:
-        f = open('players.json', 'w') #create file if it doesn't exist
+        f = open('players.json', 'w')  # create file if it doesn't exist
         f.close()
         player_data = {}
 
@@ -42,11 +59,21 @@ if __name__ == "__main__":
                        oauth=OAUTH,
                        helix=twitch.Helix(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, use_cache=True))
 
-    chat.subscribe(lambda message: handle_message(q, message))
+    handler = lambda message: handle_message(q, message)
+    chat.subscribe(handler)
 
-    json_out = json.dumps(q.player_dict)
+    choice = 0
+    while choice != 3:
+        choice = show_menu()
+        if choice == 1:
+            next_player = q.get_next_player()
+            if next_player:
+                pass
+                chat.send('@' + next_player + '. The code is: [CODE HERE]')
+            else:
+                print('No one in queue!')
 
     with open('players.json', 'w') as f:
         json.dump(q.player_dict, f)
 
-    print(json_out)
+    sys.exit(0)
